7c7502d0d222275ac22ef060029dfbcf4a897010
davi <daviyalmeiday@gmai.com>
2023-08-22 11:37:10 -0300

    da

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submit");
+    const message = document.getElementById("message");
+  
+    submitButton.addEventListener("click", async function () {
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const senha = document.getElementById("senha").value;
+  
+      const response = await fetch("/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, email, senha }),
+      });
+  
+      const result = await response.json();
+      message.textContent = result.message;
+    });
+  });
+  
