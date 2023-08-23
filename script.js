e540e99fddacb11be74f4c0c0ddff8731c57b188
Davi de Almeida Nascimento <118446616+dexterpol@users.noreply.github.com>
2023-08-23 09:37:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const submitButton = document.getElementById("submit");
-  const message = document.getElementById("message");
-
-  submitButton.addEventListener("click", async function () {
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-
-    const response = await fetch("https://cool-nougat-170998.netlify.app/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ nome, email, senha }),
+    const submitButton = document.getElementById("submit");
+    const message = document.getElementById("message");
+  
+    submitButton.addEventListener("click", async function () {
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const senha = document.getElementById("senha").value;
+  
+      const response = await fetch("/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, email, senha }),
+      });
+  
+      const result = await response.json();
+      message.textContent = result.message;
     });
-
-    const result = await response.json();
-    message.textContent = result.message;
   });
-});
+  
